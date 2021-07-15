2da12b06525a3ddec73cb0675a5bdb9cec31edb7
olzhasar <o.arystanov@gmail.com>
2021-07-16 00:47:36 +0600

    Added scoped_session db

--- db/database.py
+++ db/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 from db.dsn import POSTGRES_DSN
 
 engine = create_engine(POSTGRES_DSN, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+db = scoped_session(SessionLocal)
