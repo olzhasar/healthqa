688d9f7dadf4a6d57e3bf1d6828dc258fc4a749c
olzhasar <o.arystanov@gmail.com>
2021-07-16 00:46:33 +0600

    Crud users - added username_exists, email_exists

--- crud/users.py
+++ crud/users.py
@@ -11,8 +11,16 @@ def get_by_email(db: Session, email: str) -> Optional[User]:
     return db.query(User).filter(User.email == email).first()
 
 
+def email_exists(db: Session, email: str) -> bool:
+    return bool(db.query(User.id).filter(User.email == email).first())
+
+
 def get_by_username(db: Session, username: str) -> Optional[User]:
     return db.query(User).filter(User.username == username).first()
+
+
+def username_exists(db: Session, username: str) -> bool:
+    return bool(db.query(User.id).filter(User.username == username).first())
 
 
 def create_user(db: Session, username: str, email: str, password: str) -> User:
--- tests/crud/test_crud_user.py
+++ tests/crud/test_crud_user.py
@@ -1,7 +1,9 @@
 from crud.users import (
     create_user,
+    email_exists,
     get_by_email,
     get_by_username,
+    username_exists,
     username_or_email_taken,
 )
 from models.user import User
@@ -13,10 +15,20 @@ def test_get_by_email(db, user, other_user):
     assert get_by_email(db, "unexisting@person.com") is None
 
 
+def test_email_exists(db, user):
+    assert email_exists(db, user.email)
+    assert not email_exists(db, "unexisting@person.com")
+
+
 def test_get_by_username(db, user, other_user):
     assert get_by_username(db, user.username) == user
     assert get_by_username(db, other_user.username) == other_user != user
     assert get_by_username(db, "unexisting") is None
+
+
+def test_username_exists(db, user):
+    assert username_exists(db, user.username)
+    assert not username_exists(db, "unexisting_username")
 
 
 def test_create_user(db, user):
