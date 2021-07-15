514741870616018dfa79a030457febc73bf442cb
olzhasar <o.arystanov@gmail.com>
2021-07-15 17:24:44 +0600

    Separated views templates

--- views/home.py
+++ views/home.py
@@ -6,3 +6,8 @@ bp = Blueprint("home", __name__, template_folder="templates")
 @bp.route("/")
 def index():
     return render_template("index.html")
+
+
+@bp.route("/search")
+def search():
+    return "Not implemented"
