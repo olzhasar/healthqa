39e73f763342ba5e4c132024ad42b51b2babbe93
olzhasar <o.arystanov@gmail.com>
2021-07-15 15:11:41 +0600

    Configured static

--- app/main.py
+++ app/main.py
@@ -7,10 +7,16 @@ from views.home import bp as home_bp
 
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 TEMPLATES_DIR = BASE_DIR.joinpath("templates")
+STATIC_DIR = BASE_DIR.joinpath("static")
 
 
 def create_app():
-    app = Flask(__name__, template_folder=TEMPLATES_DIR)
+    app = Flask(
+        __name__,
+        template_folder=TEMPLATES_DIR,
+        static_folder=STATIC_DIR,
+        static_url_path="/static",
+    )
     app.config.from_object(settings)
 
     app.register_blueprint(home_bp)
