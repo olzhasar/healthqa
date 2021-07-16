9cc8897404307d708437f89b9b58ad44516e037e
olzhasar <o.arystanov@gmail.com>
2021-07-16 16:13:39 +0600

    Typing fixes

--- app/security.py
+++ app/security.py
@@ -3,14 +3,14 @@ import bcrypt
 from app.config import settings
 
 
-def hash_password(raw_password: str):
+def hash_password(raw_password: str) -> str:
     return bcrypt.hashpw(
         raw_password.encode("utf-8"),
         bcrypt.gensalt(settings.BCRYPT_ROUNDS),
     ).decode("utf-8")
 
 
-def check_password(password: str, hashed: str):
+def check_password(password: str, hashed: str) -> bool:
     return bcrypt.checkpw(
         password.encode("utf-8"),
         hashed.encode("utf-8"),
