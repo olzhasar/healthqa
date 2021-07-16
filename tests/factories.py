cfc13a3a78d286fab7f5cb5d498b427240665b52
olzhasar <o.arystanov@gmail.com>
2021-07-16 18:21:14 +0600

    UserFactory - hash_password before saving

--- tests/factories.py
+++ tests/factories.py
@@ -1,5 +1,6 @@
 import factory
 
+from app.security import hash_password
 from models.user import User
 from tests.common import TestSession
 
@@ -8,6 +9,11 @@ class UserFactory(factory.alchemy.SQLAlchemyModelFactory):
     email = factory.Faker("email")
     username = factory.Faker("first_name")
     password = factory.Faker("password")
+
+    @classmethod
+    def _save(cls, model_class, session, *args, **kwargs):
+        kwargs["password"] = hash_password(kwargs["password"])
+        return super()._save(model_class, session, *args, **kwargs)
 
     class Meta:
         model = User
