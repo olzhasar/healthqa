56e81ec850159ffcf51c476244c462b093a42ee9
olzhasar <o.arystanov@gmail.com>
2021-07-18 19:31:45 +0600

    TimeStamped fields - not nullable

--- models/mixins.py
+++ models/mixins.py
@@ -5,5 +5,7 @@ from sqlalchemy.sql.sqltypes import DateTime
 
 
 class TimeStamped:
-    created_at = Column(DateTime, default=datetime.utcnow)
-    modified_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    modified_at = Column(
+        DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
