84c2e986d2fbbdabd48a2b22b316f27f0e54ded1
olzhasar <o.arystanov@gmail.com>
2021-07-18 17:01:08 +0600

    Added Tag Model

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,9 @@
 from db.base import Base
+from models.tag import Tag
 from models.user import User
 
 __all__ = [
     "Base",
     "User",
+    "Tag",
 ]
---
+++ models/tag.py
@@ -0,0 +1,11 @@
+from sqlalchemy.sql.schema import Column
+from sqlalchemy.sql.sqltypes import Integer, String
+
+from db.base import Base
+
+
+class Tag(Base):
+    __tablename__ = "tags"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
