03fbf0258d00e6d4bd852b167ad6ec4bfe56a6bf
olzhasar <o.arystanov@gmail.com>
2021-07-18 18:55:00 +0600

    Designed factories for Tag, Question, Answer

--- tests/factories.py
+++ tests/factories.py
@@ -1,13 +1,14 @@
 import factory
+from factory.alchemy import SQLAlchemyModelFactory as BaseFactory
 
 from app.security import hash_password
-from models.user import User
+from models import Answer, Question, Tag, User
 from tests.common import TestSession
 
 
-class UserFactory(factory.alchemy.SQLAlchemyModelFactory):
+class UserFactory(BaseFactory):
     email = factory.Faker("email")
-    username = factory.Faker("first_name")
+    username = factory.Sequence(lambda n: f"user_{n}")
     password = factory.Faker("password")
 
     @classmethod
@@ -17,4 +18,34 @@ class UserFactory(factory.alchemy.SQLAlchemyModelFactory):
 
     class Meta:
         model = User
+        sqlalchemy_session = TestSession
+
+
+class TagFactory(BaseFactory):
+    name = factory.Faker("word")
+
+    class Meta:
+        model = Tag
+        sqlalchemy_session = TestSession
+
+
+class QuestionFactory(BaseFactory):
+    user = factory.SubFactory(UserFactory)
+
+    title = factory.Faker("sentence")
+    content = factory.Faker("paragraph")
+
+    class Meta:
+        model = Question
+        sqlalchemy_session = TestSession
+
+
+class AnswerFactory(BaseFactory):
+    question = factory.SubFactory(QuestionFactory)
+    user = factory.SubFactory(UserFactory)
+
+    content = factory.Faker("paragraph")
+
+    class Meta:
+        model = Answer
         sqlalchemy_session = TestSession
