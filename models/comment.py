c54325027109f9cc1d71eb1a3ab83a3ca94aec90
olzhasar <o.arystanov@gmail.com>
2021-07-25 22:15:46 +0600

    Comment revision

--- models/comment.py
+++ models/comment.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import Column, ForeignKey
 from sqlalchemy.sql.sqltypes import DateTime, Integer, Text
 
-from models.entity import UserAction
+from models.user_action import UserAction
 
 
 class Comment(UserAction):
@@ -19,9 +20,13 @@ class Comment(UserAction):
         nullable=False,
         index=True,
     )
+    user_action: UserAction = relationship(
+        "UserAction", backref="comments", foreign_keys=[user_action_id]
+    )
 
     content = Column(Text, nullable=False)
 
     __mapper_args__ = {
         "polymorphic_identity": 3,
+        "inherit_condition": id == UserAction.id,
     }
