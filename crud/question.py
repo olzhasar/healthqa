8a8e35d9b9dd4e041ea406daeb4cbea937ea3fcf
olzhasar <o.arystanov@gmail.com>
2021-07-26 00:13:13 +0600

    Updated crud/question.py

--- crud/question.py
+++ crud/question.py
@@ -1,36 +1,11 @@
-from typing import Optional
-
 from sqlalchemy.orm import joinedload
 from sqlalchemy.orm.session import Session
 
-from models.answer import Answer
-from models.comment import Comment
-from models.question import Question, QuestionVote, Tag
-from models.user import User
+from models import Question, Tag, User
 
 
 def get_by_id(db: Session, id: int):
     return db.query(Question).filter(Question.id == id).first()
-
-
-def get_for_details_view(db: Session, id: int, user: Optional[User] = None):
-    query = (
-        db.query(Question)
-        .filter(Question.id == id)
-        .options(
-            joinedload(Question.tags),
-            joinedload(Question.user).load_only("id", "name"),
-            joinedload(Question.comments).options(
-                joinedload(Comment.user).load_only("id", "name")
-            ),
-            joinedload(Question.answers).options(
-                joinedload(Answer.user).load_only("id", "name"),
-                joinedload(Answer.comments).options(joinedload(Comment.user)),
-            ),
-        )
-    )
-
-    return query.first()
 
 
 def get_all(db: Session, *, limit: int = 10):
