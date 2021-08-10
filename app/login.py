8773d7424caec6e1413a4e5ab916e403cc00de8f
olzhasar <o.arystanov@gmail.com>
2021-08-10 14:33:39 +0600

    load_user - undefer password

--- app/login.py
+++ app/login.py
@@ -1,4 +1,5 @@
 from flask_login import LoginManager
+from sqlalchemy.orm import undefer
 
 from db.database import db
 from models.user import User
@@ -9,4 +10,9 @@ login_manager.login_view = "auth.login"
 
 @login_manager.user_loader
 def load_user(user_id: str):
-    return db.query(User).filter(User.id == int(user_id)).first()
+    return (
+        db.query(User)
+        .options(undefer("password"))
+        .filter(User.id == int(user_id))
+        .first()
+    )
