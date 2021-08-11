82692a56ef93342953fdd5ce6df7c629a8fce6f7
olzhasar <o.arystanov@gmail.com>
2021-08-11 17:12:45 +0600

    Small fix

--- home/views.py
+++ home/views.py
@@ -8,6 +8,6 @@ bp = Blueprint("home", __name__, template_folder="templates")
 
 @bp.route("/")
 def index():
-    questions = crud.question.get_popular_list(db, limit=25)
+    questions = crud.question.get_popular_list(db, limit=20)
     tags = crud.tag.get_all(db)
     return render_template("index.html", questions=questions, tags=tags)
