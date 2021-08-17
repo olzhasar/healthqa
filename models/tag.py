ee07c1453c61b528a8cba62927085ef18bdb7a23
olzhasar <o.arystanov@gmail.com>
2021-08-17 15:21:02 +0600

    Added TagCategory model

--- models/tag.py
+++ models/tag.py
@@ -1,7 +1,16 @@
-from sqlalchemy.sql.schema import Column
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import Column, ForeignKey
 from sqlalchemy.sql.sqltypes import Integer, String
 
 from db.base import Base
+
+
+class TagCategory(Base):
+    __tablename__ = "tag_categories"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+    tags: list["Tag"] = relationship("Tag", back_populates="category")
 
 
 class Tag(Base):
@@ -9,3 +18,5 @@ class Tag(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False, unique=True)
+    category_id = Column(Integer, ForeignKey("tag_categories.id"), index=True)
+    category: TagCategory = relationship("TagCategory", back_populates="tags")
