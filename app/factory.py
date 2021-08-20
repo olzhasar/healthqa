3ae1a87fad469c737aded36012b0479f1810940d
olzhasar <o.arystanov@gmail.com>
2021-08-20 16:27:49 +0600

    Error handlers - fixed status code

--- app/factory.py
+++ app/factory.py
@@ -34,11 +34,11 @@ def create_app() -> Flask:
 
     @app.errorhandler(404)
     def error_404(e):
-        return render_template("404.html")
+        return render_template("404.html"), 404
 
     @app.errorhandler(500)
     def error_500(e):
-        return render_template("500.html")
+        return render_template("500.html"), 500
 
     login_manager.init_app(app)
     commands.init_app(app)
