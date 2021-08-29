67ee379858d8468a74ab4be87cee20d761078826
olzhasar <o.arystanov@gmail.com>
2021-08-29 23:55:38 +0600

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,6 @@
 
 COMPOSE="/usr/local/bin/docker-compose -f docker-compose.yml -f docker-compose.prod.yml --ansi=never"
 DOCKER="/usr/bin/docker"
-PROJECT_ROOT="/srv/healthqa"
-
-cd $PROJECT_ROOT
-
-git pull
 
 $DOCKER login $1 -u $2 -p $3
 $COMPOSE pull
