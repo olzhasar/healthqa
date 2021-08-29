72abc254d62250b23730ef004c1bcf7ab43edf6f
olzhasar <o.arystanov@gmail.com>
2021-08-29 19:40:23 +0600

    Fixed profile view. Updated tests

--- backend/src/tests/users/test_views.py
+++ backend/src/tests/users/test_views.py
@@ -18,7 +18,19 @@ class TestProfile:
         assert response.status_code == 200
         assert template_rendered("users/profile.html")
 
-    def test_non_existing(self, db: Session, client, user):
+    def test_question_tab(self, db: Session, client, question, template_rendered):
+        response = client.get(self.url.format(id=question.user.id) + "?tab=questions")
+
+        assert response.status_code == 200
+        assert template_rendered("users/profile.html")
+
+    def test_answer_tab(self, db: Session, client, answer, template_rendered):
+        response = client.get(self.url.format(id=answer.user.id))
+
+        assert response.status_code == 200
+        assert template_rendered("users/profile.html")
+
+    def test_non_existing(self, db: Session, client):
         response = client.get(self.url.format(id=999))
 
         assert response.status_code == 404
