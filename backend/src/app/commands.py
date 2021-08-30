f9fabb5aefd183306cd8d8398d7d7496efb7e4df
olzhasar <o.arystanov@gmail.com>
2021-08-30 18:39:06 +0600

    Added create_user cli command

---
+++ backend/src/app/commands.py
@@ -0,0 +1,20 @@
+import click
+from flask import Flask
+from flask.cli import with_appcontext
+
+import crud
+from db.database import db
+
+
+@click.command("create_user")
+@click.option("--email", prompt="Email")
+@click.option("--name", prompt="Display name")
+@click.option("--password", prompt="Password")
+@with_appcontext
+def create_user(email: str, name: str, password: str):
+    user = crud.user.create_user(db, email=email, name=name, password=password)
+    click.echo(f"Created user with id {user.id}")
+
+
+def init_app(app: Flask):
+    app.cli.add_command(create_user)
