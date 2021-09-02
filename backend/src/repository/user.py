d6780287281a10e222d7f84a4a6f3e3898c2f0cc
olzhasar <o.arystanov@gmail.com>
2021-09-02 19:27:44 +0600

    UserRepository - add list, create methods

--- backend/src/repository/user.py
+++ backend/src/repository/user.py
@@ -1,5 +1,7 @@
 from sqlalchemy import exc
 
+from auth.security import hash_password
+from common.pagination import Paginator
 from models.user import User
 from repository import exceptions
 from repository.base import BaseRepostitory
@@ -18,8 +20,22 @@ class UserRepository(BaseRepostitory):
         except exc.NoResultFound:
             raise exceptions.NotFoundError
 
-    def get_with_password():
-        pass
+    def count(self) -> int:
+        return self.db.query(User.id).count()
 
-    def create(self) -> User:
-        pass
+    def list(self, *, page: int = 1, per_page: int = 16) -> Paginator[User]:
+        total = self.count()
+        offset = Paginator.calc_offset(page, per_page)
+
+        objects = self.db.query(User).limit(per_page).offset(offset).all()
+
+        return Paginator(objects=objects, total=total, page=page, per_page=per_page)
+
+    def create(self, *, email: str, name: str, password: str) -> User:
+        hashed_password = hash_password(password)
+        user = User(email=email, name=name, password=hashed_password)
+
+        with self.db.begin():
+            self.db.add(user)
+
+        return user
--- backend/src/tests/repository/test_user.py
+++ backend/src/tests/repository/test_user.py
@@ -1,7 +1,9 @@
 import pytest
 
+from auth.security import check_password
 from repository import exceptions
 from repository.user import UserRepository
+from tests.factories import UserFactory
 
 pytestmark = pytest.mark.allow_db
 
@@ -27,3 +29,48 @@ class TestGetByEmail:
     def test_non_existing(self, repository: UserRepository):
         with pytest.raises(exceptions.NotFoundError):
             repository.get_by_email("non@existing.com")
+
+
+class TestList:
+    @pytest.fixture
+    def users(self):
+        return UserFactory.create_batch(4)
+
+    @pytest.mark.parametrize(
+        ("page", "per_page", "n_pages", "exp_slice"),
+        [
+            (1, 4, 1, (0, 4)),
+            (1, 2, 2, (0, 2)),
+            (2, 2, 2, (2, 4)),
+            (1, 3, 2, (0, 3)),
+        ],
+    )
+    def test_page(
+        self, users, repository: UserRepository, page, per_page, n_pages, exp_slice
+    ):
+        paginator = repository.list(page=page, per_page=per_page)
+
+        assert paginator.objects == users[slice(*exp_slice)]
+        assert paginator.total == len(users)
+        assert paginator.page == page
+        assert paginator.per_page == per_page
+        assert len(paginator) == n_pages
+
+
+class TestCreate:
+    @pytest.fixture
+    def data(self):
+        return {
+            "email": "info@test.com",
+            "name": "Test User",
+            "password": "123qweasd",
+        }
+
+    def test_ok(self, repository: UserRepository, data):
+        user = repository.create(**data)
+
+        from_db = repository.get(user.id)
+        assert from_db == user
+        assert from_db.email == data["email"]
+        assert from_db.name == data["name"]
+        assert check_password(data["password"], from_db.password)
