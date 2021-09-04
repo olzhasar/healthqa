a972eb80afb247435f331a8189ce74fecef2591b
olzhasar <o.arystanov@gmail.com>
2021-09-04 16:26:20 +0600

    Clean BaseRepostitory

--- backend/src/repository/base.py
+++ backend/src/repository/base.py
@@ -1,15 +1,10 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, NoReturn, Optional, TypeVar
+from typing import TYPE_CHECKING, TypeVar
 
 from models import Base
-from storage import Store
-from storage import store as main_store
 
 if TYPE_CHECKING:
-    from redis import Redis
-    from sqlalchemy.orm.session import Session
-
     from common.pagination import Paginator
 
 
@@ -21,21 +16,8 @@ class BaseRepostitory:
     Class for isolating data sources activities
     """
 
-    store: Store
-
-    def __init__(self, store: Optional[Store] = None) -> NoReturn:
-        self.store = store or main_store
-
-    @property
-    def db(self) -> Session:
-        return self.store.db
-
-    @property
-    def redis(self) -> Redis:
-        return self.store.redis
-
     def get(self, id: int) -> ModelType:
         raise NotImplementedError
 
-    def list(self, limit: int, offset: int) -> Paginator[ModelType]:
+    def list(self, page: int, per_page: int) -> Paginator[ModelType]:
         raise NotImplementedError
